61ae692dc62f3b88bfc71beb82383b4771b05e6a
Semih <23503571+semihsmg@users.noreply.github.com>
2022-01-29 18:27:36 +0300

    add comment

--- hotspot_status.py
+++ hotspot_status.py
@@ -42,15 +42,17 @@ reset_dict(report_list)
 while True:
     for index, sn in enumerate(serial_numbers):
         response = requests.get(url + '&sn=' + sn)
-        response_json = response.json()
+        response_json = response.json() # get hotspot informations
 
+        # strip json and only get necessary info for quick review email
         name = response_json["data"]["name"]
         address = response_json["data"]["address"]
         synced_value = response_json["data"]["synced"]
         online_value = response_json["data"]["online"]
         relayed_value = True if response_json["data"]["relayed"] == 1 else False
 
-        if synced_value == False or online_value == False or relayed_value == True:
+        if synced_value == False or online_value == False or relayed_value == True:  # enter if the hotspot got issue
+            # can be disabled if you want to run it in the background
             # print info of out of synced, offline or relayed hotspot(s)
             time_stamp()
             print(name)
@@ -62,7 +64,7 @@ while True:
             print('------------------------------')
             # end info
 
-            if report_list[index] == 0:
+            if report_list[index] == 0:  # change mail body to this hotspot's informations
                 body += \
                     '<a href=' + explorer_url + address + '>' + name + '</a>' + '\n' + \
                     "synced: " + str(synced_value) + '\n' + \
@@ -70,23 +72,23 @@ while True:
                     "relayed: " + str(relayed_value) + '\n' + \
                     '<a href=' + dashboard_url + sn + '>Dashboard</a>' + '\n\n'
 
-            report_list[index] += 1
+            report_list[index] += 1 # increase issue counter of hotspot
 
-            if report_list[index] >= 4:
+            if report_list[index] >= 4:  # within 2 hours if hotspot still not online then add that hotspot name to a list and send reminder email
                 names += name + ','
 
     if len(body) > 0:
         yag.send(to=to, subject=subject, contents=body)
         body = ''
 
+    if len(names) > 0: # if names list contains any name send reminder email
+        subject2 = names + ' still down!'
+        yag.send(to=to, subject=subject2, contents='')
+
     reset_report_counter += 1
-    if reset_report_counter == 8:
+    if reset_report_counter == 8:  # after 4 hours reset report_list to send email of all hotspots that needs attention
         reset_report_counter = 0
         for key in report_list:
             reset_dict(report_list)
-
-    if len(names) > 0:
-        subject2 = names + ' still down!'
-        yag.send(to=to, subject=subject2, contents='')
 
     time.sleep(seconds - ((time.time() - start_time) % seconds))
